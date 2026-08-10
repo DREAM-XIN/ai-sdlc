570bed1529a66734a11a8a795fc1934a062f222c
DREAM-XIN <33620907+DREAM-XIN@users.noreply.github.com>
2026-08-10 13:53:32 +0800

    test(operator): run vertical gh-aw correlation validator

--- scripts/validate.py
+++ scripts/validate.py
@@ -17,6 +17,7 @@ from validate_operator_mcp import main as validate_operator_mcp
 from validate_operator_store import main as validate_operator_store
 from validate_operator_store_runtime import main as validate_operator_store_runtime
 from validate_operator_vertical import main as validate_operator_vertical
+from validate_operator_vertical_gh_aw import main as validate_operator_vertical_gh_aw
 from validate_operator_vertical_recovery import main as validate_operator_vertical_recovery
 from validate_remediation_review_completion import main as validate_remediation_review_completion
 from validate_transition import validate_schema as validate_execution_schema
@@ -217,6 +218,7 @@ def main():
     validate_operator_store_runtime()
     validate_operator_vertical()
     validate_operator_vertical_recovery()
+    validate_operator_vertical_gh_aw()
     errors = (
         validate_schemas()
         + validate_profiles()
